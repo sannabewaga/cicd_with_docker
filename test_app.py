1914c65a6dd3f9a7d1cafbbf6c98139500c6138a
sannabewaga <gauravsarthak00@gmail.com>
2025-06-24 17:57:52 +0530

    byte string error fix

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_home():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data== 'hello world'
+    assert response.data== b'hello world'
